d812390a41c73c1ae82cbc2f6f9ef9db4cdd1d5c
lhoutre <lhoumeau.maxime@gmail.com>
2023-08-08 10:50:57 +0200

    add URL to prod env file

--- ffkmda-front/src/environments/environment.prod.ts
+++ ffkmda-front/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  url:'http://pprod-club.ffkmda.fr:9008'
 };
+
+
+/*
+ * URL !
+ * PPROD : http://prod-club.ffkmda.fr:9008
+ * PROD : http://pprod-club.ffkmda.fr:9008
+ * LOCAL : http://localhost:9008
+ */
